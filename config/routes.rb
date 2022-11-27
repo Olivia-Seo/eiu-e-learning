f32d69e34e621977b22363f01d90ecf0b920d3b8
Olivia <oliviaseo7799@gmail.com>
2022-11-27 16:04:34 +0000

    simple form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root "static_pages#landing_page"
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
-  get "privacy_policy", to: "static_pages#privacy_policy"
+  devise_for :users
+  resources :courses
+  get 'home/index'
+  root "home#index"
+  get 'home/index'
+  get 'home/privacy_policy'
+  get "privacy_policy", to: "home#privacy_policy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
