e14f2d0a82bfa2cf28ff9fe31d97abf0d89fcf9c
Olivia <oliviaseo7799@gmail.com>
2022-12-02 10:12:04 +0000

    ex mailers

---
+++ app/mailers/ApplicationHelper.rb
@@ -0,0 +1,15 @@
+ class ApplicationMailer < ActionMailer::Base
+   default from: 'support@eiu-e-learning.herokuapp.com'
+   layout 'mailer'
+end
+
+# class UserNotifierMailer < ApplicationMailer
+#   default :from => 'support@eiu-e-learning.herokuapp.com'
+
+#   # send a signup email to the user, pass in the user object that   contains the user's email address
+#   def send_signup_email(user)
+#     @user = user
+#     mail( :to => @user.email,
+#     :subject => 'Thanks for signing up for our amazing app' )
+#   end
+# end
