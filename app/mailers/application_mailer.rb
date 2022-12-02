931838abb3a5c1d83e569ff8d0d811535577caf1
Olivia <oliviaseo7799@gmail.com>
2022-12-02 09:50:05 +0000

    add mmore details info to course

--- app/mailers/application_mailer.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'support@eiu-e-learning.herokuapp.com'
-  layout 'mailer'
-end
--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,22 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.sendgrid.net', 
+#   :port => '587', 
+#   :authentication => :plain, 
+#   :user_name => ENV['SENDGRID_USERNAME'], 
+#   :password => ENV['SENDGRID_PASSWORD'], 
+#   :domain => 'heroku.com', 
+#   :enable_starttls_auto => true 
+# }
+
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net', 
-  :port => '587', 
-  :authentication => :plain, 
-  :user_name => ENV['SENDGRID_USERNAME'], 
-  :password => ENV['SENDGRID_PASSWORD'], 
-  :domain => 'heroku.com', 
-  :enable_starttls_auto => true 
+  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  :password => '<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>', # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'eiu-e-learning.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
