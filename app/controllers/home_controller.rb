0ea66c37bd5184e41252c9b65a195d0e5fec8325
Olivia <oliviaseo7799@gmail.com>
2022-12-04 16:14:10 +0000

    add footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,9 @@ class HomeController < ApplicationController
     @courses = Course.all.limit(3)
     @latest_couses = Course.all.limit(3).order(created_at: :desc)
   end
-
-  def privacy_policy
+  
+  def activity
+    @activities = PublicActivity::Activity.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :courses
   resources :users, only: [:index]
   get 'home/index'
+  get 'home/activity'
   root "home#index"
   get 'home/index'
-  get 'home/privacy_policy'
-  get "privacy_policy", to: "home#privacy_policy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
