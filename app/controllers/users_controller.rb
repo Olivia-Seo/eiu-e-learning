1375a027076330520434398c76bd3cb8545a81b6
Olivia <oliviaseo7799@gmail.com>
2022-12-13 07:38:33 +0000

    add rolify and putdit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+ 
+  before_action :set_user, only: [:edit, :update]
+ 
   def index
       #@users = User.all.order(created_at: :desc )
       
@@ -6,6 +9,26 @@ class UsersController < ApplicationController
        @users = @q.result(distinct: true)
   end
   
+   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User roles were successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({role_ids: []})
+  end
   #def create
     # Create the user from params
    # @user = User.new(user_params)
