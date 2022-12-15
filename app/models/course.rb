7c5d6fdd6a27d6341310d5672f6d4f56f3fe1ec5
Olivia <oliviaseo7799@gmail.com>
2022-12-15 10:54:41 +0000

    add lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ApplicationRecord
   validates :title,  presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   belongs_to :user
+  has_many :lessons, dependent: :destroy
+
   def to_s
     title
   end
