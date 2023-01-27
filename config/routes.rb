27f79d0c75b828f82957dd9b16c98a929444e7a0
Olivia <oliviaseo7799@gmail.com>
2023-01-27 04:40:31 +0000

    add video from youtube

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     
     resources :enrollments, only: [:new, :create]
   end
+  resources :youtube, only: :show
   resources :users, only: [:index, :edit, :show, :update]
   get 'home/index'
   get 'activity', to: 'home#activity'
